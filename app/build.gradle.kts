2dcd8154cd1bb6f7a9c4766f92ce413ccf13c1fb
Vaibhav Lakhera <vaibhav.lakhera@gmail.com>
2024-08-20 23:06:00 +0530

    - Updated to version 1.0.1 (2)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.minimo.launcher"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
     }
 
     buildFeatures {
